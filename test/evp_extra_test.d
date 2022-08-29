c30c1cc4727b8df080b98a09c937442980e36a2a
unknown <fochoao@macprolate2013.mit.edu>
2022-08-29 14:10:44 -0500

    OpenSSL 1.1.1q x64 for Windows, all files.

---
+++ test/evp_extra_test.d
@@ -0,0 +1,113 @@
+evp_extra_test.c
+Note: including file: C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\stdio.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt.h
+Note: including file:   C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.32.31326\include\vcruntime.h
+Note: including file:    C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.32.31326\include\sal.h
+Note: including file:     C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.32.31326\include\concurrencysal.h
+Note: including file:    C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.32.31326\include\vadefs.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt_wstdio.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt_stdio_config.h
+Note: including file: C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\stdlib.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt_malloc.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt_search.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\stddef.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt_wstdlib.h
+Note: including file:  C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.32.31326\include\limits.h
+Note: including file: C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\string.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt_memory.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt_memcpy_s.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\errno.h
+Note: including file:    C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.32.31326\include\vcruntime_string.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt_wstring.h
+Note: including file: C:\openssl-1.1.1q\include\openssl/aes.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/opensslv.h
+Note: including file: C:\openssl-1.1.1q\include\openssl/bio.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/e_os2.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:   C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.32.31326\include\stdint.h
+Note: including file:  C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.32.31326\include\stdarg.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/crypto.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\time.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\corecrt_wtime.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/safestack.h
+Note: including file:    C:\openssl-1.1.1q\include\openssl/stack.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/ossl_typ.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/cryptoerr.h
+Note: including file:    C:\openssl-1.1.1q\include\openssl/symhacks.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/bioerr.h
+Note: including file: C:\openssl-1.1.1q\include\openssl/err.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/lhash.h
+Note: including file: C:\openssl-1.1.1q\include\openssl/evp.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/evperr.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/objects.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/obj_mac.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/asn1.h
+Note: including file:    C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:    C:\openssl-1.1.1q\include\openssl/asn1err.h
+Note: including file:    C:\openssl-1.1.1q\include\openssl/bn.h
+Note: including file:     C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:     C:\openssl-1.1.1q\include\openssl/bnerr.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/objectserr.h
+Note: including file: C:\openssl-1.1.1q\include\openssl/rsa.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/rsaerr.h
+Note: including file: C:\openssl-1.1.1q\include\openssl/x509.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/buffer.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/buffererr.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.22621.0\ucrt\sys/types.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/ec.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/ecerr.h
+Note: including file:    C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/dsa.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/dh.h
+Note: including file:    C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:    C:\openssl-1.1.1q\include\openssl/dherr.h
+Note: including file:     C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/dsaerr.h
+Note: including file:    C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/sha.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/x509err.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/x509_vfy.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/pkcs7.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/pkcs7err.h
+Note: including file: C:\openssl-1.1.1q\include\openssl/pem.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/pemerr.h
+Note: including file: C:\openssl-1.1.1q\include\openssl/kdf.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/kdferr.h
+Note: including file: C:\openssl-1.1.1q\include\openssl/engine.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/rand.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/randerr.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/ui.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/uierr.h
+Note: including file:  C:\openssl-1.1.1q\include\openssl/engineerr.h
+Note: including file:   C:\openssl-1.1.1q\include\openssl/opensslconf.h
+Note: including file: C:\openssl-1.1.1q\test\testutil.h
+Note: including file: C:\openssl-1.1.1q\include\internal/nelem.h
+Note: including file: C:\openssl-1.1.1q\include\crypto/evp.h
+Note: including file:  C:\openssl-1.1.1q\include\internal/refcount.h
+test\evp_extra_test.c(493): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(553): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(559): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(574): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(640): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(656): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(726): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(752): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(761): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(786): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(1080): warning C4267: 'function': conversion from 'size_t' to 'long', possible loss of data
+test\evp_extra_test.c(1141): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(1221): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(1526): warning C4267: 'function': conversion from 'size_t' to 'long', possible loss of data
+test\evp_extra_test.c(1533): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+test\evp_extra_test.c(1540): warning C4267: 'function': conversion from 'size_t' to 'long', possible loss of data
+test\evp_extra_test.c(1639): warning C4267: 'function': conversion from 'size_t' to 'long', possible loss of data
+test\evp_extra_test.c(1648): warning C4267: 'function': conversion from 'size_t' to 'long', possible loss of data
